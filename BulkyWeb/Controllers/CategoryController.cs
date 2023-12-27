cb6f3ec9b266c8674b2787a75a60e0b53917f746
hirotoe0112 <hirotoe0112@gmail.com>
2023-12-27 09:57:12 +0900

    display categories

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,13 +1,21 @@
+using BulkyWeb.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         // GET: CategoryController
         public ActionResult Index()
         {
-            return View();
+            var categories = _db.Categories.ToList();
+            return View(categories);
         }
 
     }
